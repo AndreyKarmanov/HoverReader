e592fa4ff7290a43f406e880c1bb6d3ee2d2e449
Andrey Karmanov <64520339+inflatedhat2@users.noreply.github.com>
2021-04-21 13:10:04 -0400

    Basic and quick setup.

--- background.js
+++ background.js
@@ -1,24 +1,37 @@
-//This script runs ONE time at the start of the function
-//if you go into your 'extensions' page on chrome there should be a 'background page' that you can click on, the console will go to this
-console.log("Background")
+// //This script runs ONE time at the start of the function
+// //if you go into your 'extensions' page on chrome there should be a 'background page' that you can click on, the console will go to this
+// console.log("Background")
 
-// sets the local storage with address 'teststorage' to 'test' 
-chrome.storage.local.set({
-  teststorage: 'test'
-});
+// // sets the local storage with address 'teststorage' to 'test' 
+// chrome.storage.local.set({
+//   teststorage: 'test'
+// });
 
-//gets the local storage from 'teststorage' and prints it to the console
-chrome.storage.local.get(['teststorage'], function (result) {
-  console.log('Value is: ', result.teststorage)
-});
+// //gets the local storage from 'teststorage' and prints it to the console
+// chrome.storage.local.get(['teststorage'], function (result) {
+//   console.log('Value is: ', result.teststorage)
+// });
 
-//when a message is sent from another script, it goes through this listener
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  //checks if message matches
-  if (request.message === 'storage updated') {
-    chrome.storage.local.get(['teststorage'], function (result) {
-      //prints out the value from the storage
-      console.log(result.teststorage)
-    });
-  }
+// //when a message is sent from another script, it goes through this listener
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//   //checks if message matches
+//   if (request.message === 'storage updated') {
+//     chrome.storage.local.get(['teststorage'], function (result) {
+//       //prints out the value from the storage
+//       console.log(result.teststorage)
+//     });
+//   }
+// });
+
+
+function getword(info,tab) {
+  console.log("Word " + info.selectionText + " was clicked.");
+  chrome.tabs.create({  
+    url: "https://translate.google.com/?sl=auto&tl=en&text=" + info.selectionText.split(" ").join("+")
+  });
+}
+chrome.contextMenus.create({
+  title: "Search: %s", 
+  contexts:["selection"], 
+  onclick: getword
 });
