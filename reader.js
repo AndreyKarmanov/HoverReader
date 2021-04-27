d8b5bca93b89ef1282350415f53e9d2fd1fc4e74
Andrey Karmanov <Andrey@karmanov.ca>
2021-04-27 17:01:50 -0400

    styles and such
    base styles, unsure how to handle the options/animations just yet.

--- reader.js
+++ reader.js
@@ -1,17 +1,21 @@
 var index = 0;
 var wording = [];
 var options = {};
-var readDisplay = document.getElementById('read');
+var readDisplay = document.getElementById('readDisplay');
+var optionsDisplay = document.getElementById('optionsDisplay');
 var speedDisplay = document.getElementById('speed');
-var speed = 500;
+var optionsShow = document.getElementById('optionsShow');
+
+var speed = 200;
 
 chrome.storage.local.get(['text'], (result) => {
     wording = result.text.split(" ");
     chrome.storage.local.get(['options'], (result) => {
         options = result.options;
         console.log(result)
-        setTimeout(readWord, speed);
+        setTimeout(readWord, ms(speed));
         console.log("started reading")
+        speedDisplay.innerText = options.speed;
     });
 });
 
@@ -19,23 +23,35 @@ chrome.storage.local.get(['text'], (result) => {
 function readWord() {
     readDisplay.innerText = wording[index++]
     if (index < wording.length) {
-        setTimeout(readWord, speed);
+        setTimeout(readWord, ms(speed));
     };
 };
 
-function wpm(speed){
+function ms(speed) {
     return parseInt(60000 / speed)
 }
+
 document.onkeypress = function (e) {
     e = e || window.event;
     console.log(e.key, e);
 };
 
 document.onwheel = function (e) {
-    if (e.deltaY > 0 && speed < 1000){
-        speed += options.scrollSens;
-    }else if (e.deltaY < 0  && speed > 120){
+    if (e.deltaY > 0 && speed > 100) {
         speed -= options.scrollSens;
+    } else if (e.deltaY < 0 && speed < 1000) {
+        speed += options.scrollSens;
     };
-    speedDisplay.innerText = wpm(speed);
+    speedDisplay.value = speed;
+};
+
+optionsShow.onclick = function (e) {
+    console.log('the dispaly', optionsDisplay.style.display)
+    if (optionsDisplay.style.visibility == 'hidden'){
+        // optionsDisplay.style.paddingTop = '100px';
+        optionsDisplay.style.visibility = 'visible';
+    }else{
+        optionsDisplay.style.visibility = 'hidden';
+        // optionsDisplay.style.paddingTop = '100px';
+    };
 };
